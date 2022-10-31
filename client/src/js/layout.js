5026cc9e7aede2b0ce3a0dcd50f7426055f53a9e
Jessica Rojas Mosquera <jeess.rm1@gmail.com>
2022-10-31 10:34:18 +0100

    Add store and context API (Flux)

--- client/src/js/layout.js
+++ client/src/js/layout.js
@@ -1,13 +1,29 @@
-import React from 'react';
-import { Navbar } from './components/Navbar'
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
+import { Home } from "./views/home";
 
+import injectContext from "./store/appContext";
 
+import { Navbar } from "./components/navbar";
+import { Footer } from "./components/footer";
 
-export default function App() {
-  return (
-    <div id="light" className='h-100'>
-      <Navbar />			
-    </div>
-  );
-}
+const Layout = () => {
+    /* const basename = process.env.BASENAME || ""; */
+
+    return (
+        <div>
+            <BrowserRouter /* basename={basename} */>
+                    <Navbar />
+                    <Routes>
+                        <Route element={<Home />} path="/" />
+
+                        <Route element={<h1>Not found!</h1>} />
+                    </Routes>
+                    <Footer />
+            </BrowserRouter>
+        </div>
+    );
+};
+
+export default injectContext(Layout);
---
+++ client/src/js/store/flux.js
@@ -0,0 +1,35 @@
+const getState = ({ getStore, getActions, setStore }) => {
+	return {
+		store: {
+			message: null,
+			demo: [
+				{
+					title: "FIRST",
+					background: "white",
+					initial: "white"
+				},
+				{
+					title: "SECOND",
+					background: "white",
+					initial: "white"
+				}
+			]
+		},
+		actions: {
+			/* getMessage: async () => {
+				try{
+					// fetching data from the backend
+					const resp = await fetch(process.env.BACKEND_URL + "/api/hello")
+					const data = await resp.json()
+					setStore({ message: data.message })
+					// don't forget to return something, that is how the async resolves
+					return data;
+				}catch(error){
+					console.log("Error loading message from backend", error)
+				}
+			} */
+		}
+	};
+};
+
+export default getState;
