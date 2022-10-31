9ad56c6b7126ba4cd720c2cc1f75ac5642574468
Jessica Rojas Mosquera <jeess.rm1@gmail.com>
2022-10-31 12:55:40 +0100

    Fix MIME type error

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -11,7 +11,8 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "[name][contenthash].js",
-    clean: true
+    clean: true,
+    publicPath: '/'
   },
   devtool: "source-map",
   devServer: {
